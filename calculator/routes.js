54409900ee6b3242a70ea2933b838203974e5d8c
Sonika Gupta <sonikag0209@gmail.com>
2023-08-08 20:26:26 -0600

    First Commit

---
+++ calculator/routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+// bring in our controller logic
+const calcController = require("./controller");
+
+const router = express.Router();
+
+router.get("/:bill/:tipPer", calcController.show);
+
+// index route  // localhost:8080/fruits/
+router.get("/", calcController.index);
+
+// example of a "show" route/ returns 1 item
+router.get("/:question", calcController.show);
+
+module.exports = router;
